9f18d9cfa9f202525f78336f8ace85abd721617a
Scravers <jamestephen@shaw.ca>
2024-11-23 22:43:49 -0700

    Updated to use bot instead of observation as function paramerter

--- utility.cpp
+++ utility.cpp
@@ -1,8 +1,11 @@
 #include "utility.h"
 
+#include "BasicSc2Bot.h"
+
 // Get the available supply
-float getAvailableSupply(const sc2::ObservationInterface* observation) {
-    float total_supply = observation->GetFoodCap();
-    float used_supply = observation->GetFoodUsed();
+float getAvailableSupply(BasicSc2Bot& bot) {
+    float total_supply = bot.observation->GetFoodCap();
+    float used_supply = bot.observation->GetFoodUsed();
     return total_supply - used_supply;
 }
+
--- utility.h
+++ utility.h
@@ -3,7 +3,20 @@
 
 #include "header_files.h"
 
+
 // Get available supply
-float getAvailableSupply(const sc2::ObservationInterface* observation);
+float getAvailableSupply(BasicSc2Bot& bot);
+
+// https://github.com/Blizzard/s2client-api/blob/614acc00abb5355e4c94a1b0279b46e9d845b7ce/examples/common/bot_examples.cc#L62C1-L76C3
+struct IsTownHall {
+    bool operator()(const sc2::Unit& unit) {
+        switch (unit.unit_type.ToType()) {
+            case sc2::UNIT_TYPEID::ZERG_HATCHERY: return true;
+            case sc2::UNIT_TYPEID::ZERG_LAIR: return true;
+            case sc2::UNIT_TYPEID::ZERG_HIVE : return true;
+            default: return false;
+        }
+    }
+};
 
 #endif
