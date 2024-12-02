75dce40f4f78bc80fa87be8d8c66a2286b2b3ab1
Scravers <jamestephen@shaw.ca>
2024-12-02 14:46:18 -0700

    Added wait for 10 frames to find expansions

--- BasicSc2Bot.cpp
+++ BasicSc2Bot.cpp
@@ -22,10 +22,18 @@ void BasicSc2Bot::OnGameStart() {
 
     unit_manager.OnGameStart(*this);
 
-
 }
 
 void BasicSc2Bot::OnStep() {
+
+    static int step_counter = 0;
+
+    ++step_counter;
+    // Wait for 10 frames
+    if (step_counter < 10) {
+        return;
+    }
+
     // Switch case for all the steps we will do
     switch (state_machine.current_state) {
 
@@ -49,46 +57,46 @@ void BasicSc2Bot::OnStep() {
             state_machine.FirstExpansionState(*this);
             break;
 
-        // case StateMachineManager::POST_FIRST_EXPANSION:
-        //     // std::cout << "Post Expansion State" << std::endl;
-        //     state_machine.PostFirstExpansionState(*this);
-        //     break;
+        case StateMachineManager::POST_FIRST_EXPANSION:
+            // std::cout << "Post Expansion State" << std::endl;
+            state_machine.PostFirstExpansionState(*this);
+            break;
 
-        // case StateMachineManager::QUEENING:
-        //     // std::cout << "Queen State" << std::endl;
-        //     state_machine.QueeningState(*this);
-        //     attack.ScoutWithOverlord(*this);
-        //     break;
+        case StateMachineManager::QUEENING:
+            // std::cout << "Queen State" << std::endl;
+            state_machine.QueeningState(*this);
+            attack.ScoutWithOverlord(*this);
+            break;
 
-        // case StateMachineManager::MORE_EXTRACTING:
-        //     // std::cout << "More Extracting State" << std::endl;
-        //     state_machine.MoreExtractingState(*this);
-        //     break;
-        // case StateMachineManager::SATURATE_EXTRACTORS:
-        //     // std::cout << "Saturate Extractors State" << std::endl;
-        //     state_machine.SaturateExtractorsState(*this);
-        //     break;
+        case StateMachineManager::MORE_EXTRACTING:
+            // std::cout << "More Extracting State" << std::endl;
+            state_machine.MoreExtractingState(*this);
+            break;
+        case StateMachineManager::SATURATE_EXTRACTORS:
+            // std::cout << "Saturate Extractors State" << std::endl;
+            state_machine.SaturateExtractorsState(*this);
+            break;
 
-        // case StateMachineManager::ROACH_WARREN:
-        //     state_machine.RoachWarrenState(*this);
-        //     break;
+        case StateMachineManager::ROACH_WARREN:
+            state_machine.RoachWarrenState(*this);
+            break;
 
-        // case StateMachineManager::BASE_DEFENSE:
-        //     state_machine.BaseDefenseState(*this);
-        //     break;
+        case StateMachineManager::BASE_DEFENSE:
+            state_machine.BaseDefenseState(*this);
+            break;
 
-        // case StateMachineManager::SECOND_EXPANSION:
-        //     state_machine.SecondExpansionState(*this);
-        //     break;
+        case StateMachineManager::SECOND_EXPANSION:
+            state_machine.SecondExpansionState(*this);
+            break;
 
-        // case StateMachineManager::ROACHPOCALYPSE:
-        //     state_machine.RoachpocalypseState(*this);
-        //     break;
+        case StateMachineManager::ROACHPOCALYPSE:
+            state_machine.RoachpocalypseState(*this);
+            break;
 
-        // case StateMachineManager::ATTACK:
-        //     state_machine.AttackState(*this);
-        //     attack.RoachRush(*this);
-        //     break;
+        case StateMachineManager::ATTACK:
+            state_machine.AttackState(*this);
+            attack.RoachRush(*this);
+            break;
     }
 }
 
